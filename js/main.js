459bc23243ce497708bd708c4e1e62126b692381
LeQuocHoaiDuc22130048 <22130048@st.hcmuaf.edu.vn>
2025-09-14 16:20:21 +0700

    add feature paginantion when screen small

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 import { partnerLogos, productList, partnerLogoBasePath } from "./data.js";
 
+// Nav
+$(function () {
+  //hide show nav
+  $(".nav").hideScroll();
+
+  //mobile menu
+  const toggleBtn = $("#toggle_btn");
+  const dropdownMenu = $(".dropdown-menu");
+
+  toggleBtn.click(() => {
+    dropdownMenu.toggleClass("open");
+  });
+});
+
 // Partner Logos
 $(function () {
   const container = document.getElementById("partner-logo-list");
@@ -10,5 +24,21 @@ $(function () {
     img.alt = logo.alt;
     img.classList.add("logo-ticket-image");
     container.appendChild(img);
+  });
+});
+
+// Products
+$(function () {
+  // Thêm active và li đầu tiên
+  $("li:first").addClass("activeTab");
+
+  // Đổi màu activeTab
+  $("li").on("click", function () {
+    $("li").removeClass("activeTab");
+    $("div[id=products-tabs] ul .r-tabs-state-active").addClass("activeTab");
+  });
+
+  $("#products-tabs").responsiveTabs({
+    animation: "slide",
   });
 });
